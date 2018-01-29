e1cd63bc8e0f0f734a4007d44e3de6c4d51dbd0f
user <contact@stevegravy.com>
2018-01-29 16:51:58 +0100

    nav barre

--- Multipage/lje/src/nav.js
+++ Multipage/lje/src/nav.js
@@ -4,7 +4,13 @@ import './nav.css';
 class nav extends Component {
   render() {
     return (
-
+      <div>
+        <ul>
+          <li><link to="./presentation">Présentation Mini-Entreprises</li>
+          <li><link to="./actualites">Actualités des Mini-Entreprises</li>
+          <li><link to="./merci">Merci</li>
+        </ul>
+      </div>
     );
   }
 }
