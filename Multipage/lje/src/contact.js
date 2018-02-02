979d9626f07f4ae8815337cb53b706c10868b9b7
omar <omaraaiachi1@gmail.com>
2018-02-02 10:56:01 +0100

    ajout de contact minientreprise2016218 header

---
+++ Multipage/lje/src/contact.js
@@ -0,0 +1,174 @@
+import React from 'react'
+
+var Contact = React.createClass({
+    render: function() {
+      return (
+        <div>
+        <p><strong><span style={{fontSize: '18pt'}}>Découvrez les coordinateurs Mini-Entreprises</span></strong></p>
+        <p><strong>&nbsp;</strong></p>					
+        <div className="items-leading">
+          <div className="leading leading-0">
+            {/* Article */}
+            <article>
+              {/* Aside */}
+              {/* //Aside */}
+              <section className="article-intro clearfix">
+                <table style={{width: '100%'}} cellPadding={0} border={0}>
+                  <tbody>
+                    <tr>
+                      <td colSpan={3}><span style={{fontSize: '18pt'}}><strong>Coordinateur du Brabant Wallon</strong></span></td>
+                    </tr>
+                    <tr>
+                      <td style={{width: '33%'}} valign="middle"><img src="/images/Systeme/Contacts%20Personnel/Contact-Mini/Frederic-200x200.png" alt="Frédéric Mesmaker - Coordinateur de la province du Brabant Wallon" style={{marginRight: 5, float: 'left'}} title="Frédéric Mesmaker - Coordinateur de la province du Brabant Wallon" width={200} height={200} /></td>
+                      <td style={{width: '34%'}} valign="top">
+                        <p>&nbsp;</p>
+                        <p><strong>Frédéric Mesmaker</strong></p>
+                        <p><br /><span id="cloak256d5ea4cbc1176fce5e0e8dd6e70a56"><a href="mailto:frederic@lje.be">frederic@lje.be</a></span></p>
+                        <p>0475/ 56 02 29</p>
+                      </td>
+                      <td style={{width: '33%'}} valign="top">
+                        <p>
+                          {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) starts here */}
+                        </p><div className="avPlayerWrapper avVideo">
+                          <div style={{width: 400}} className="avPlayerContainer">
+                            <div id="AVPlayerID_0_9df6a831098dedff967974804f811903" className="avPlayerBlock">
+                              <iframe src="https://www.youtube.com/embed/DngJuJUO3Qw?rel=0&fs=1&wmode=transparent" allowFullScreen="true" scrolling="no" title="JoomlaWorks AllVideos Player" width={400} height={250} frameBorder={0} />					</div>
+                          </div>
+                        </div>
+                        {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) ends here */}
+                        <p />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>		</section>
+              {/* footer */}
+              {/* //footer */}
+            </article>
+            {/* //Article */}
+          </div>
+        </div>{/* end items-leading */}
+        <div className="items-row cols-1 row-0 row">
+          <div className="col-sm-12">
+            <div className="item column-1">
+              {/* Article */}
+              <article>
+                {/* Aside */}
+                {/* //Aside */}
+                <section className="article-intro clearfix">
+                  <table style={{width: '100%'}} cellPadding={0} border={0}>
+                    <tbody>
+                      <tr>
+                        <td colSpan={3}><span style={{fontSize: '18pt'}}><strong>Coordinatrice du Hainaut </strong></span></td>
+                      </tr>
+                      <tr>
+                        <td style={{width: '33%'}}><img src="/images/Systeme/Contacts%20Personnel/Contact-Mini/Clara-200x200-Arrondi.png" alt="Clara Jates - Coordinatrice de la province du Hainaut" style={{marginRight: 5, float: 'left'}} title="Clara Jates - Coordinatrice de la province du Hainaut" /></td>
+                        <td style={{width: '34%'}} valign="top">
+                          <p>&nbsp;</p>
+                          <p><strong>Clara Jates</strong></p>
+                          <p><br /><span id="cloak1da616161edc6f999999ba480f9c2373"><a href="mailto:clara@lje.be">clara@lje.be</a></span></p>
+                          <p>0494/ 874 106</p>
+                        </td>
+                        <td style={{width: '33%'}} valign="top">
+                          <p>
+                            {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) starts here */}
+                          </p><div className="avPlayerWrapper avVideo">
+                            <div style={{width: 400}} className="avPlayerContainer">
+                              <div id="AVPlayerID_0_c666a27051a71b3c7025dae8e768ce61" className="avPlayerBlock">
+                                <iframe src="https://www.youtube.com/embed/t7e34mOkcZI?rel=0&fs=1&wmode=transparent" allowFullScreen="true" scrolling="no" title="JoomlaWorks AllVideos Player" width={400} height={250} frameBorder={0} />					</div>
+                            </div>
+                          </div>
+                          {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) ends here */}
+                          <p />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>		</section>
+                {/* footer */}
+                {/* //footer */}
+              </article>
+              {/* //Article */}
+            </div>{/* end item */}
+          </div>{/* end span */}
+        </div>{/* end row */}
+        <div className="items-row cols-1 row-1 row">
+          <div className="col-sm-12">
+            <div className="item column-1">
+              {/* Article */}
+              <article>
+                {/* Aside */}
+                {/* //Aside */}
+                <section className="article-intro clearfix">
+                  <table style={{width: '100%'}} cellPadding={0} border={0}>
+                    <tbody>
+                      <tr>
+                        <td colSpan={3}><span style={{fontSize: '18pt'}}><strong>Coordinateur de Liège et du Luxembourg</strong></span></td>
+                      </tr>
+                      <tr>
+                        <td style={{width: '33%'}}><img src="/images/Systeme/Contacts%20Personnel/Contact-Mini/Nicolas-200x200.png" alt="Nicolas Godefroid - Coordinateur de la province de Liège et du Luxembourg" style={{marginRight: 5, float: 'left'}} title="Nicolas Godefroid - Coordinateur de la province de Liège et du Luxembourg" /></td>
+                        <td style={{width: '34%'}} valign="top">
+                          <p>&nbsp;</p>
+                          <p><strong>Nicolas Godefroid</strong></p>
+                          <p><br /><span id="cloak5c22b1d4c3b8f071b894e613f8605521"><a href="mailto:nicolas@lje.be">nicolas@lje.be</a></span></p>
+                          <p>0491/ 500 314</p>
+                        </td>
+                        <td style={{width: '33%'}} valign="top">
+                          <p>
+                            {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) starts here */}
+                          </p><div className="avPlayerWrapper avVideo">
+                            <div style={{width: 400}} className="avPlayerContainer">
+                              <div id="AVPlayerID_0_878c7eb248cc98af55462c921b137cfd" className="avPlayerBlock">
+                                <iframe src="https://www.youtube.com/embed/fUZx2hrWkYo?rel=0&fs=1&wmode=transparent" allowFullScreen="true" scrolling="no" title="JoomlaWorks AllVideos Player" width={400} height={250} frameBorder={0} />					</div>
+                            </div>
+                          </div>
+                          {/* JoomlaWorks "AllVideos" Plugin (v4.8.0) ends here */}
+                          <p />
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table>		</section>
+                {/* footer */}
+                {/* //footer */}
+              </article>
+              {/* //Article */}
+            </div>{/* end item */}
+          </div>{/* end span */}
+        </div>{/* end row */}
+        <div className="items-row cols-1 row-2 row">
+          <div className="col-sm-12">
+            <div className="item column-1">
+              {/* Article */}
+              <article>
+                {/* Aside */}
+                {/* //Aside */}
+                <section className="article-intro clearfix">
+                  <table style={{width: '100%'}} cellPadding={0} border={0}>
+                    <tbody>
+                      <tr>
+                        <td colSpan={3}><span style={{fontSize: '18pt'}}><strong>Coordinateur de Namur </strong></span></td>
+                      </tr>
+                      <tr>
+                        <td style={{width: '33%'}}><img src="/images/Systeme/Contacts%20Personnel/Contact-Mini/Robert-200x200.png" alt="Robert Dion - Coordinateur de la province de Namur" style={{marginRight: 5, float: 'left'}} title="Robert Dion - Coordinateur de la province de Namur" /></td>
+                        <td style={{width: '34%'}} valign="top">
+                          <p>&nbsp;</p>
+                          <p><strong>Robert Dion</strong></p>
+                          <p><br /><span id="cloak985bcb7f08c5e850c7792038444a2227"><a href="mailto:robert@lje.be">robert@lje.be</a></span></p>
+                          <p>0475/ 822 500</p>
+                        </td>
+                        <td style={{width: '33%'}} valign="top">
+                          <p>&nbsp;</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>		</section>
+              </article></div></div></div></div>
+      );
+    }
+  });
+
+
+export default Contact;
---
+++ Multipage/lje/src/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Header{
+    render(){
+        return(
+            <div>
+                <input placeholder="votre adresse mail"></input>
+                <button>S'enregistrer</button>
+            </div>
+        )
+    }
+}
+
+export default Header;
