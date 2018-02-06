ea166891fffb0becee500689df48162411627b3a
jimmyGoossens <jimmy.v.goossens@gmail.com>
2018-02-06 09:37:02 +0100

    Add files via upload

---
+++ Multipage/lje/src/component/footer.js
@@ -0,0 +1,134 @@
+import React from 'react';
+
+
+export default class Footer extends React.Component {
+
+	render(){
+
+		return(
+
+			<section>
+			<div id= "contact">
+							<h2>CONTACT</h2>
+
+				<p>Les Jeunes Entreprises ASBL
+				Rue Dr Elie Lambotte 10 - 1030 Bruxelles</p>
+				 
+				 <p> 02/245 13 80</p>
+				 
+				<p> mail </p>
+				 
+				<p>N°de compte LJE</p>
+				 
+				<p>IBAN: BE66 2100 4390 1343</p>
+			</div>
+			<div id="recherche">
+				RECHERCHER SUR LE SITE
+				Rechercher
+			</div>
+			<div id="follow">
+				<h2>REJOIGNEZ-NOUS SUR:</h2>
+					<img src="" alt=""/>
+					<img src="" alt=""/>
+					<img src="" alt=""/>
+					<img src="" alt=""/>
+
+			</div>
+
+
+</section>
+<section  id="soutiens">
+<h2> Entreprises public</h2>
+	<div id="public">
+		<img src="marshall.png" alt=""/>
+		<img src="wallo.png" alt=""/>
+		<img src="bx_capital.jpg" alt=""/>
+		<img src="yet.jpg" alt=""/>
+		<img src="enmieux.png" alt=""/>
+	</div>
+	<h2>Entreprises Privés</h2>
+
+	<div id="prive">
+		<img src="bnp.png" alt=""/>
+		<img src="pulse.jpg" alt=""/>
+		<img src="lotterie.jpg" alt=""/>
+		<img src="at&t.png" alt=""/>
+		<img src="deloitte.jpg" alt=""/>
+		<img src="solvay.jpg" alt=""/>
+		<img src="accenture.png" alt=""/>
+		<img src="euroclear.png" alt=""/>
+		<img src="fedEx.png" alt=""/>
+		<img src="ucm.png" alt=""/>
+		<img src="ccv.png" alt=""/>
+		<img src="mufg.jpg" alt=""/>
+		<img src="e_soir.png" alt=""/>
+		
+	</div>
+
+
+	<div>
+
+		<ul>
+			<li>Accueil	</li>
+			<li>Contact</li>
+			<li>Agenda</li>
+			<li>QUI SOMMES NOUS</li>
+			<li>Vision missions et valeurs</li>
+			<li>Objectifs</li>
+			<li>Junior Achievement Europe</li>
+			<li>Equipe LJE</li>
+			<li>Conseil d'Administration</li>
+			<li>Rapports d'activités</li>
+			<li>PROGRAMMES</li>
+			<li>Notre Commune</li>
+
+		</ul>
+		<ul>
+			<li>Présentation Notre Commune</li>
+			<li>Inscription</li>
+			<li>Plate-forme Notre Commune</li>
+			<li>Concours</li>
+			<li>Contact Notre Commune</li>
+			<li>C Ma Ville</li>
+			<li>Présentation C Ma Ville</li>
+			<li>Contact C Ma ville</li>
+			<li>Mini-Entreprise</li>
+			<li>Présentation Mini-Entreprises</li>
+			<li>Actualités des Mini-Entreprises</li>
+			<li>Les Mini-Entreprises 2017-2018</li>
+
+		</ul>
+		<ul>
+			<li>Les Mini-Entreprises 2016-2017</li>
+			<li>Les coordinateurs du programme Mini-Entreprise</li>
+			<li>Les outils en ligne</li>
+			<li>Inscription</li>
+			<li>Contact Mini-Entreprise</li>
+			<li>ESP - Entrepreneurial Skills Pass</li>
+			<li>Young Enterprise Project</li>
+			<li>Présentation du YEP</li>
+			<li>Actualités du YEP</li>
+			<li>YEP Day</li>
+			<li>YEP Challenge</li>
+			<li>Circular challenge</li>
+		</ul>
+
+	</div>
+
+</section>
+
+
+
+
+
+
+
+
+						);
+
+
+	}
+
+
+
+}
