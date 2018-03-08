3cef6c66541e5a8a5c63a960e622285e6a4bc3f4
Unknown <gjeloshaj.antoine@gmail.com>
2018-03-08 09:32:15 +0100

    Ajout du backend en php

---
+++ CMS Backoffice/server php/index.php
@@ -0,0 +1,31 @@
+<?php
+$f3 = require('fatfree/lib/base.php');
+
+$f3->route('GET /',function() {
+  echo \Template::instance()->render('index.html');
+
+});
+$f3->route('GET /admin',function() {
+  echo \Template::instance()->render('index.html');
+
+});
+
+$f3->set('ONERROR',function($f3){
+  //echo \Template::instance()->render('error.html');
+  // $info = parse_url($_SERVER['REQUEST_URI']);
+   http_response_code(200);
+
+   if (file_exists( "./$info[path]")) {
+     //echo("Content-Type:".mime_content_type('./'.$info["path"]));
+    // $finfo = finfo_open(FILEINFO_MIME_TYPE);
+     //echo finfo_file($finfo, "./$info[path]");
+     header("Content-Type:".mime_content_type("./$info[path]"));
+     readfile("./$info[path]");
+   }else{
+     echo \Template::instance()->render('index.html');
+   }
+});
+
+
+$f3->run();
+?>
